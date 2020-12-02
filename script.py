9df0e46a18b6727292eaa2cb2d6c8aa53275963a
viktor <dangerouzua@gmail.com>
2020-12-02 10:59:56 +0200

    Added logs for ignored playlist

--- script.py
+++ script.py
@@ -454,18 +454,18 @@ def update_files(now):
         if not playlist_id.startswith("37i9dQZF1E37YIfAiHUTYF"):
             plain_path = "{}/{}".format(plain_dir, playlist_id)
             try:
-                test = spotify.get_playlist(ignored)
+                test = spotify.get_playlist(playlist_id.startswith("37i9dQZF1E37YIfAiHUTYF"))
                 print('Test: {}'.format(test))
             except:
                 print("Error: {}".format(traceback.format_exc()))
             else:
                 print('Test: {}'.format(test.name))
                 readme_lines.append(
-                        "- [{}]({})".format(
-                            test.name,
-                            URL.pretty(test.name),
-                        )
+                    "- [{}]({})".format(
+                        test.name,
+                        URL.pretty(test.name),
                     )
+                )
             try:
                 playlist = spotify.get_playlist(playlist_id)
             except PrivatePlaylistError:
